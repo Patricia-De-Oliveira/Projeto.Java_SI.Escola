36e8f68fe12cbc45782ab6a1aa3fb7391f0fe145
LuccaPCDoria <doria.lucca@gmail.com>
2026-05-28 17:56:33 -0300

    subindo o Service e Controller da classe Notas

--- src/controller/NotaController.java
+++ src/controller/NotaController.java
@@ -1,4 +1,24 @@
 package controller;
+import model.Nota;
+import service.NotaService;
+import java.util.List;
 
 public class NotaController {
+    private final NotaService notaService = new NotaService();
+    // Chamando metodo cadastrar
+    public void cadastrarNota(Nota nota){
+        notaService.cadastrarNota(nota);
+    }
+    // Chamando metodo listar
+    public List<Nota> listarNotas(){
+        return notaService.listarNotas();
+    }
+    // Chamando metodo atualizar
+    public String atualizarNota(String matricula, double novoValor) {
+        return notaService.atualizarNota(matricula, novoValor);
+    }
+    // Chamando metodo deletar
+    public String deletarNota(String matricula) {
+        return notaService.deletarNota(matricula);
+    }
 }
--- src/service/NotaService.java
+++ src/service/NotaService.java
@@ -1,5 +1,38 @@
 package service;
 
-public class NotaService{
+import java.util.ArrayList;
+import java.util.List;
+import model.Nota;
 
+public class NotaService{
+    private final List<Nota> notas = new ArrayList<>();
+
+    //MétodoPOST - Cadastro
+    public void cadastrarNota(Nota nota) {
+        notas.add(nota);
+    }
+    //MétodoGet - Listar
+    public List<Nota> listarNotas() {
+        return notas;
+    }
+    //MétodoPUT - Atualizar
+    public String atualizarNota(String matricula,double novoValor) {
+        for(Nota nota : notas) {
+            if(nota.getAluno().getMatricula().equalsIgnoreCase(matricula)){
+                nota.setValor(novoValor);
+                return "Nota alterada com sucesso!";
+            }
+        }
+        return "Aluno não encontrado!";
+    }
+    //MétodoDELETE - Deletar
+    public String deletarNota(String matricula) {
+        for(int i = 0; i < notas.size(); i++) {
+            if(notas.get(i).getAluno().getMatricula().equalsIgnoreCase(matricula)) {
+                notas.remove(i);
+                return "Nota deletada com sucesso!";
+            }
+        }
+        return "Aluno não encontrado!";
+    }
 }
