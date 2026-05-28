193ba28fb1a92bcacd0bcd08310b92ec2f05bec2
Karlla Luz <karllaluzlana@gmail.com>
2026-05-28 19:38:36 -0300

    subindo alteracoes no service

--- src/controller/ProfessorController.java
+++ src/controller/ProfessorController.java
@@ -1,4 +1,27 @@
 package controller;
 
+import model.Professor;
+import service.ProfessorService;
+
 public class ProfessorController {
+
+    private final ProfessorService service =
+            new ProfessorService();
+
+    public void cadastrarProfessor(
+            String matricula,
+            String nome,
+            String disciplina){
+    }
+
+    public void listarProfessores(){
+
+        for(Professor p :
+                service.listarProfessores()){
+
+            System.out.println(p);
+        }
+
+    }
+
 }
--- src/controller/TurmaController.java
+++ src/controller/TurmaController.java
@@ -1,4 +1,56 @@
 package controller;
 
+import model.Turma;
+import service.TurmaService;
+
 public class TurmaController {
+
+    private final TurmaService service =
+            new TurmaService();
+
+    public void cadastrarTurma(
+            String codigo,
+            String nome,
+            int semestre){
+    }
+
+    public void listarTurmas(){
+
+        for(Turma turma :
+                service.listarTurmas()){
+
+            System.out.println(
+                    turma
+            );
+
+        }
+
+    }
+
+    public void atualizarTurma(
+            String codigo,
+            String nome,
+            int semestre){
+
+        System.out.println(
+                service.atualizarTurma(
+                        codigo,
+                        nome,
+                        semestre
+                )
+        );
+
+    }
+
+    public void deletarTurma(
+            String codigo){
+
+        System.out.println(
+                service.deletarTurma(
+                        codigo
+                )
+        );
+
+    }
+
 }
