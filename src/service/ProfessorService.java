b7c9db7dc8228e255c1101d629e0dd2abcd03fd7
LuccaPCDoria <doria.lucca@gmail.com>
2026-05-29 14:52:28 -0300

    fix: Restruturação do service e controller de professor

--- src/service/ProfessorService.java
+++ src/service/ProfessorService.java
@@ -9,12 +9,36 @@ public class ProfessorService {
 
     private final List<Professor> professores = new ArrayList<>();
 
+    // CREATE
     public void cadastrarProfessor(Professor professor){
         professores.add(professor);
     }
 
+    // READ
     public List<Professor> listarProfessores(){
         return professores;
     }
 
+    // UPDATE
+    public String atualizarProfessor(String matricula, String novoNome){
+        for(Professor professor : professores){
+            if(professor.getMatricula().equalsIgnoreCase(matricula)){
+                professor.setNome(novoNome);
+                return "Professor atualizado!";
+            }
+        }
+
+        return "Professor não encontrado!";
+    }
+
+    // DELETE
+    public String deletarProfessor(String matricula){
+        for(int i = 0; i < professores.size(); i++){
+            if(professores.get(i).getMatricula().equalsIgnoreCase(matricula)){
+                professores.remove(i);
+                return "Professor removido!";
+            }
+        }
+        return "Professor não encontrado!";
+    }
 }
